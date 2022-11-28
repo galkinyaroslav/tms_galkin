b25e91ae228fe4549dfe03a9faf1347cdf36abe4
Yaroslav Galkin <galkinyaroslav@gmail.com>
2022-11-28 20:40:31 +0300

    changed lesson4_1

--- Lesson4/lesson4_1.py
+++ Lesson4/lesson4_1.py
@@ -12,6 +12,31 @@ def my_list_reverse(old_list: list) -> list:
     return new_list
 
 
+def my_list_reverse_v2(old_list: list) -> list:
+    new_list = [0] * len(old_list)
+    idx = -1
+    for _ in range(len(old_list)):
+        new_list[-idx - 1] = old_list[idx]
+        idx -= 1
+    return new_list
+
+
+def my_list_reverse_v3(old_list: list) -> list:
+    new_list = []
+    idx = -1
+    for _ in range(len(old_list)):
+        new_list.append(old_list[idx])
+        idx -= 1
+    return new_list
+
+
+def my_list_reverse_v4(old_list: list) -> list:
+    new_list = []
+    for idx in range(len(old_list)):
+        new_list.insert(0, old_list[idx])
+    return new_list
+
+
 my_list = list(range(11, 61))
 # my_reverse_list = [my_list[-idx - 1] for idx, val in enumerate(my_list)]
 # print(my_reverse_list)
@@ -21,3 +46,9 @@ my_list = list(range(11, 61))
 # print(my_reverse_list2)
 my_reverse_list3 = my_list_reverse(my_list)
 print(f'my_reverse_list3>>>{my_reverse_list3}')
+my_reverse_list4 = my_list_reverse_v2(my_list)
+print(f'my_reverse_list4>>>{my_reverse_list4}')
+my_reverse_list5 = my_list_reverse_v3(my_list)
+print(f'my_reverse_list5>>>{my_reverse_list5}')
+my_reverse_list6 = my_list_reverse_v4(my_list)
+print(f'my_reverse_list6>>>{my_reverse_list6}')
