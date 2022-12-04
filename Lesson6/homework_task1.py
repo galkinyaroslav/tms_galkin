126e99d18888abe5560d661986d09fd91fb7011d
Yaroslav Galkin <galkinyaroslav@gmail.com>
2022-12-04 19:40:38 +0300

    add lesson6, done 6_1

---
+++ Lesson6/homework_task1.py
@@ -0,0 +1,23 @@
+"""
+Написать рекурсивную функцию, которая будет принимать на вход список целых чисел
+и возвращать инвертированную копию этого списка (рекурсивная реализация функции reverse)
+"""
+
+def recursive_reverse(source_list: list)-> list:
+    if not source_list:
+        return source_list
+    else:
+        return [source_list[-1]]+recursive_reverse(source_list[:-1])
+
+
+
+
+"""
+Test
+"""
+
+source = [1, 2, 3, 4, 5]
+print(f'source{source}')
+print(recursive_reverse(source))
+assert recursive_reverse(source) == [5, 4, 3, 2, 1]
+
---
+++ Lesson6/homework_task2.py
@@ -0,0 +1,18 @@
+"""
+Написать рекурсивную функцию, которая принимат на вход список целых чисел
+и возвращает максимальное число в списке
+"""
+
+def recursive_max(some_list):
+    """
+    Your code is here
+    """
+    pass
+
+"""
+Test
+"""
+
+source = [2, 1, 0, 5, 7, 6, 4, 3]
+print(source)
+assert recursive_max(source) == 7
